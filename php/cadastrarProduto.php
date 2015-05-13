372f850eaa2f46d50fbd92ba7e59e81e4b18fd87
lw4z <ivanvalentimsantos@gmail.com>
2015-05-13 20:53:59 -0300

    Ajuste no sql

--- php/cadastrarProduto.php
+++ php/cadastrarProduto.php
@@ -1,27 +1,28 @@
 <?php
 
-
- if (isset($_POST['nome'])) {
 ob_start();
+ if (isset($_POST['nome'])) {
 
-$nome = $_POST['nome'];
-$descricao =$_POST['descricao'];
-$categoria = $_POST['categoria'];
-$marca = $_POST['marca'];
+    $nome = $_POST['nome'];
+    $descricao =$_POST['descricao'];
+    $categoria = $_POST['categoria'];
+    $marca = $_POST['marca'];
 
 
 include("../util/connection.php");
 include ("../php/upload.php");
 
-$result = mysql_query(" insert into produto(nome_produto,detalhes_produto,marca_produto,categoria_produto) values ('$nome','$descricao','$marca,'$categoria')");
+    $result = mysql_query("insert into produto(nome_produto,detalhes_produto,categoria_produto,marca_produto) values ('$nome','$descricao','$categoria','$marca')");
 
 
-echo '<h3>Produto Cadastrado!</h3><br />';
-echo 'Nome: '.$nome_final."<br />";
+    echo '<h3>Produto Cadastrado!</h3><br />';
+    echo 'Nome: '.$nome_final."<br />";
 
 
 }
-///UPLOAD
+
+?>
+
 
 
 
