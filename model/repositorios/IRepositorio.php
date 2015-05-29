a2d254d6f3f6657922bdb7df2f75ac572e4b60d8
Edward_Hill <Carlo5_3duardo@hotmail.com>
2015-05-29 16:56:44 -0300

    sexta

--- model/repositorios/IRepositorio.php
+++ model/repositorios/IRepositorio.php
@@ -1,5 +1,7 @@
 <?php
 
+
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -7,17 +9,15 @@
  */
 
 /**
- *
+ *puli===
  * @author Edward_Hill
  */
-
 interface IRepositorio {
     
-    public function adcionar($entidade);
-    public function editar($entidade);
-    public function pesquisar();
-    public function listar();
-    public function remover($entidade);
+    public function adcionar(Usuario $entidade);
+    public function editar(Usuario $entidade);
+    public function pesquisar(Usuario $entidade);
+    public function remover(Usuario $entidade);
         
 }
 
--- model/repositorios/RepositorioUsuario.php
+++ model/repositorios/RepositorioUsuario.php
@@ -1,39 +1,65 @@
 <?php
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  * Description of RepositorioUsuario
  *
  * @author Edward_Hill
  */
-class RepositorioUsuario implements IRepositorio {
+include ('IRepositorio.php');
+class RepositorioUsuario implements IRepositorio{
     
-   
-
-    public function adcionar($entidade) {
-       
+    public function adcionar(\Usuario $entidade) {
+        
+      $nome = $entidade->getNome();
+      $email = $entidade->getEmail();
+      $senha = $entidade->getSenha();
+      $telefone = $entidade->getTelefone();
+      
+     include("../util/connection.php");
+    $result = mysql_query("insert into usuario(nome_usuario,email_usuario,senha_usuario,telefone_usuario) values ('$nome','$email','$senha','$telefone')");
+    session_start();
+    $_SESSION["mensagem"] = "Usuário Cadastrado!";
+	header('Location:../index.php');
+     
     }
-
-    public function editar($entidade) {
+    
+      public function pesquisar(\Usuario $entidade) {
+        
+         if($entidade->getId_usuario()>0){ 
+       include("../util/connection.php");
+           $id = $entidade->getId_usuario();
+           
+           $dados = mysql_query(" SELECT *  FROM usuario  WHERE id_usuario = '"+$id+"'");
+                           while( $sql = mysql_fetch_array($dados)){
+                                $id = $sql['id_usuario'];
+                               $nome = $sql['nome_usuario'];
+			       $email = $sql['email_usuario'];
+                               $telefone = $sql['telefone_usuario'];
+                               $senha  = $sql['senha_usuario'];
+                               $usuario = new Usuario($id, $nome, $email, $senha, $telefone); 
+                                return $usuario;
+                           }
+ 
+         }
+        
         
     }
-
-    public function listar() {
+    
+    
+    public function editar(\Usuario $entidade) {
+        
+        include("../util/connection.php");
+        $result = mysql_query("UPDATE usuario  SET nome_usuario ='" + $entidade->getNome()+"',email_usuario ='"+$entidade->getEmail()+"',senha_usuario = '"+ $entidade->getSenha()+"'");
         
     }
-
-    public function pesquisar() {
+  
+    public function remover(\Usuario $entidade) {
         
+        include("../util/connection.php");        
+        $result = mysql_query("DELETE  ON usuario  WHERE id_usuario ='" + $entidade->getId_usuario()+"'");
     }
-
-    public function remover($entidade) {
-        
-    }
-
-//put your code here
 }
