a6c628b216a00532e2226d6a9fc58041b5575234
Jackson <jackson.daniel.almeida@gmail.com>
2015-05-31 12:02:53 -0300

    Comitando SQL

--- model/repositorios/sql/opinemais.sql
+++ model/repositorios/sql/opinemais.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.3.11
+-- version 4.2.11
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 14-Maio-2015 às 05:44
--- Versão do servidor: 5.6.24
--- PHP Version: 5.6.8
+-- Generation Time: 31-Maio-2015 às 12:01
+-- Versão do servidor: 5.6.21
+-- PHP Version: 5.6.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -23,15 +23,40 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `comentario`
+--
+
+CREATE TABLE IF NOT EXISTS `comentario` (
+`id_comentario` int(11) NOT NULL,
+  `mensagem` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `comentario_opiniao_usuario`
+--
+
+CREATE TABLE IF NOT EXISTS `comentario_opiniao_usuario` (
+  `id_usuario` int(11) NOT NULL DEFAULT '0',
+  `id_opiniao` int(11) NOT NULL DEFAULT '0',
+  `id_comentario` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `opiniao`
 --
 
 CREATE TABLE IF NOT EXISTS `opiniao` (
-  `id_opiniao` int(11) NOT NULL,
-  `id_produto` int(11) NOT NULL,
-  `titulo_opiniao` varchar(50) DEFAULT NULL,
-  `descricao_opiniao` longtext NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+`id_opiniao` int(11) NOT NULL,
+  `mensagem` varchar(500) NOT NULL,
+  `qualificacao` varchar(10) NOT NULL,
+  `nota` float DEFAULT NULL,
+  `id_usuario` int(11) DEFAULT NULL,
+  `id_produto` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -40,94 +65,111 @@ CREATE TABLE IF NOT EXISTS `opiniao` (
 --
 
 CREATE TABLE IF NOT EXISTS `produto` (
-  `id_produto` int(11) NOT NULL,
-  `nome_produto` varchar(200) NOT NULL,
-  `detalhes_produto` longtext NOT NULL,
-  `marca_produto` varchar(50) NOT NULL,
-  `categoria_produto` varchar(200) NOT NULL,
-  `imagem_produto` varchar(200) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
-
---
--- Extraindo dados da tabela `produto`
---
-
-INSERT INTO `produto` (`id_produto`, `nome_produto`, `detalhes_produto`, `marca_produto`, `categoria_produto`, `imagem_produto`) VALUES
-(1, 'Carlos Eduardo', 'dssd', 'ewewe', 'Tv', ''),
-(2, 'Carlos Eduardo', 'dssd', 'ewewe', 'Tv', ''),
-(3, 'Carlos Eduardo', 'dssd', 'ewewe', 'Tv', ''),
-(4, 'Motorola moto Maxx', 'Moto Maxx , 3G de RAM, 64 Gigas de memoria interna, tela QHD, camera 21 megapixels', 'Motorola', 'cosmeticos', '1431561577.jpg'),
-(5, 'dfdf', 'dfdf', 'dfdfd', 'Celulares e SmartPhones', '1431561706.jpg');
+`id_produto` int(11) NOT NULL,
+  `nome_produto` varchar(50) NOT NULL,
+  `detalhes` varchar(500) NOT NULL,
+  `imagem` varchar(100) NOT NULL,
+  `qualificacao_positiva` int(11) DEFAULT NULL,
+  `qualificacao_negativa` int(11) DEFAULT NULL,
+  `nota_media` float DEFAULT NULL,
+  `id_usuario` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `resposta`
+-- Estrutura da tabela `usuario`
 --
 
-CREATE TABLE IF NOT EXISTS `resposta` (
-  `id_resposta` int(11) NOT NULL,
-  `id_opiniao` int(11) NOT NULL,
-  `titulo_resposta` varchar(50) NOT NULL,
-  `descricao_resposta` longtext NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `usuario` (
+`id_usuario` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `senha` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `comentario`
+--
+ALTER TABLE `comentario`
+ ADD PRIMARY KEY (`id_comentario`);
+
+--
+-- Indexes for table `comentario_opiniao_usuario`
+--
+ALTER TABLE `comentario_opiniao_usuario`
+ ADD PRIMARY KEY (`id_usuario`,`id_opiniao`,`id_comentario`), ADD KEY `id_opiniao` (`id_opiniao`), ADD KEY `id_comentario` (`id_comentario`);
+
+--
 -- Indexes for table `opiniao`
 --
 ALTER TABLE `opiniao`
-  ADD PRIMARY KEY (`id_opiniao`), ADD KEY `id_produto` (`id_produto`);
+ ADD PRIMARY KEY (`id_opiniao`), ADD KEY `id_usuario` (`id_usuario`), ADD KEY `id_produto` (`id_produto`);
 
 --
 -- Indexes for table `produto`
 --
 ALTER TABLE `produto`
-  ADD PRIMARY KEY (`id_produto`);
+ ADD PRIMARY KEY (`id_produto`), ADD KEY `id_usuario` (`id_usuario`);
 
 --
--- Indexes for table `resposta`
+-- Indexes for table `usuario`
 --
-ALTER TABLE `resposta`
-  ADD PRIMARY KEY (`id_resposta`), ADD KEY `id_opiniao` (`id_opiniao`);
+ALTER TABLE `usuario`
+ ADD PRIMARY KEY (`id_usuario`), ADD UNIQUE KEY `email` (`email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `comentario`
+--
+ALTER TABLE `comentario`
+MODIFY `id_comentario` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `opiniao`
 --
 ALTER TABLE `opiniao`
-  MODIFY `id_opiniao` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id_opiniao` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `produto`
 --
 ALTER TABLE `produto`
-  MODIFY `id_produto` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+MODIFY `id_produto` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `resposta`
+-- AUTO_INCREMENT for table `usuario`
 --
-ALTER TABLE `resposta`
-  MODIFY `id_resposta` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `usuario`
+MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- Constraints for dumped tables
 --
 
 --
+-- Limitadores para a tabela `comentario_opiniao_usuario`
+--
+ALTER TABLE `comentario_opiniao_usuario`
+ADD CONSTRAINT `comentario_opiniao_usuario_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`) ON DELETE CASCADE,
+ADD CONSTRAINT `comentario_opiniao_usuario_ibfk_2` FOREIGN KEY (`id_opiniao`) REFERENCES `opiniao` (`id_opiniao`) ON DELETE CASCADE,
+ADD CONSTRAINT `comentario_opiniao_usuario_ibfk_3` FOREIGN KEY (`id_comentario`) REFERENCES `comentario` (`id_comentario`) ON DELETE CASCADE;
+
+--
 -- Limitadores para a tabela `opiniao`
 --
 ALTER TABLE `opiniao`
-ADD CONSTRAINT `opiniao_ibfk_1` FOREIGN KEY (`id_produto`) REFERENCES `produto` (`id_produto`);
+ADD CONSTRAINT `opiniao_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`) ON DELETE CASCADE,
+ADD CONSTRAINT `opiniao_ibfk_2` FOREIGN KEY (`id_produto`) REFERENCES `produto` (`id_produto`) ON DELETE CASCADE;
 
 --
--- Limitadores para a tabela `resposta`
+-- Limitadores para a tabela `produto`
 --
-ALTER TABLE `resposta`
-ADD CONSTRAINT `resposta_ibfk_1` FOREIGN KEY (`id_opiniao`) REFERENCES `opiniao` (`id_opiniao`);
+ALTER TABLE `produto`
+ADD CONSTRAINT `produto_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`) ON DELETE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
