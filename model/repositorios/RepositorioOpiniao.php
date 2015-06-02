bdaccd489592f282ba5ef49899eba7b333df3f01
Jackson <jackson.daniel.almeida@gmail.com>
2015-06-01 23:13:53 -0300

    Finalizando repositório de opiniao

--- model/repositorios/RepositorioOpiniao.php
+++ model/repositorios/RepositorioOpiniao.php
@@ -18,8 +18,8 @@ class RepositorioOpiniao implements IRepositorio{
         $result = mysql_query("insert into opiniao(mensagem,qualificacao,nota,id_usuario,id_produto) "
                 . "values "
                 . "('".$entidade->getMensagem()."',"
-                . "'".$entidade->getQualificacao."',"
-                . $entidade->getNota.","
+                . "'".$entidade->getQualificacao()."',"
+                . $entidade->getNota().","
                 . $entidade->getUsuario()->getId_usuario().","
                 . $entidade->getProduto()->getId_produto().")");
         echo $result;
@@ -31,19 +31,45 @@ class RepositorioOpiniao implements IRepositorio{
                 . "mensagem = '".$entidade->getMensagem()."', "
                 . "qualificacao = '".$entidade->getQualificacao()."', "
                 . "nota = ".$entidade->getNota()
-                . "");
+                . "where id_opiniao = ".$entidade->getId_opiniao());
     }
 
     public function listar(\Opiniao $entidade) {
-        
+        $result = mysql_query('select * from opiniao');
+        while( $sql = mysql_fetch_array($result)){
+            $id_opiniao = $sql['id_opiniao'];
+            $mensagem = $sql['mensagem'];
+            $qualificacao = $sql['qualificacao'];
+            $nota = $sql['nota'];
+            $id_usuario = $sql['id_usuario'];
+            $id_produto = $sql['id_produto'];
+            
+            $usuario = new Usuario($id_usuario);
+            $produto = new Produto($id_produto);
+            $opiniao = new Opiniao($id_opiniao, $mensagem, $qualificacao, $nota, $usuario, $produto);
+        }
+        return $opiniao;
     }
 
     public function pesquisar(\Opiniao $entidade) {
-        
+        $result = mysql_query('select * from opiniao where id_opiniao = '.$entidade->getId_opiniao());
+        while( $sql = mysql_fetch_array($result)){
+            $id_opiniao = $sql['id_opiniao'];
+            $mensagem = $sql['mensagem'];
+            $qualificacao = $sql['qualificacao'];
+            $nota = $sql['nota'];
+            $id_usuario = $sql['id_usuario'];
+            $id_produto = $sql['id_produto'];
+            
+            $usuario = new Usuario($id_usuario);
+            $produto = new Produto($id_produto);
+            $opiniao = new Opiniao($id_opiniao, $mensagem, $qualificacao, $nota, $usuario, $produto);
+        }
+        return $opiniao;  
     }
 
     public function remover(\Opiniao $entidade) {
-        
+        $result = mysql_query('delete from opiniao where id_opiniao = '.$entidade->getId_opiniao());
     }
 
 //put your code here
