c6fd0ad5cad113122f507331ee4231e619d10204
Edward_Hill <Carlo5_3duardo@hotmail.com>
2015-06-07 13:44:34 -0300

    tabedas

--- home.php
+++ home.php
@@ -4,7 +4,7 @@
     <head>
         <meta charset="UTF-8">
         <title>Home - Opine Mais </title>
-        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">           
        	<script type="text/javascript" src="js/jquery.js"></script>
         <script type="text/javascript" src="js/easySlider1.7.js"></script>
         <script type="text/javascript" src="js/script.js"></script>
@@ -102,7 +102,7 @@
         ?>	
                         <li>
                             <a href="detalharProduto.php?id_produto=<?php echo $produtoDown->getId_produto();?>" class="image">
-                                <img src="images/upload/<?php echo $produtoDown->getImagem();?>" alt="<?php echo $produtoDown->getCategoria();?>" /></a>
+                                <img src="images/upload/<?php echo $produtoDown->getImagem();?>" id="imabaixa"  alt="<?php echo $produtoDown->getCategoria();?>" /></a>
                                 <h2>
                                     <?php 
                                         echo $produtoDown->getNome_produto();
@@ -119,73 +119,7 @@
                 }
             }
         ?>    
-        <li>
-            <a href="#" class="image"><img src="css/images/pq21435686.jpg" alt="Game" /></a>
-        	<h2>Grand Theft Auto</h2>
-            <p>R$ 120,00</p>
-        </li>
-        <li>
-            <a href="#" class="image"><img src="css/images/pq21440232.jpg" alt="Game" /></a>
-        	<h2>Need for Speed</h2>
-            <p>R$ 95,00</p>
-        </li>
-        <li>
-            <a href="#" class="image"><img src="css/images/pq21465797.jpg" alt="Game" /></a>
-        	<h2>Mortal Kombat</h2>
-            <p>R$ 110,00</p>
-        </li>
-        <li>
-            <a href="#" class="image"><img src="css/images/pq21465871.jpg" alt="Game" /></a>
-        	<h2>Devil May Cry</h2>
-            <p>R$ 90,00</p>
-        </li>
-        <li>
-             <a href="#" class="image"><img src="css/images/pq21478377.jpg" alt="Game" /></a>
-        	<h2>Resident Evil 5</h2>
-            <p>R$ 150,00</p>
-        </li>
-        <li>
-            <a href="#" class="image"><img src="css/images/pq21480470.jpg" alt="Game" /></a>
-        	<h2>Lord of the Rings</h2>
-            <p>R$ 130,00</p>
-        </li>
-        <li>
-            <a href="#" class="image"><img src="css/images/pq21498024.jpg" alt="Game" /></a>
-        	<h2>Kill Zone 2</h2>
-            <p>R$ 99,00</p>
-        </li>
-        <li>
-            <a href="#" class="image"><img src="css/images/pq21507480.jpg"alt="Game" /></a>
-        	<h2>Tomb Raider</h2>
-            <p>R$ 140,00</p>
-        </li>
-    
-        <li>
-            <a href="#" class="image"><img src="css/images/pq21507483.jpg"alt="Game" /></a>
-        	<h2>Wanted</h2>
-            <p>R$ 125,00</p>
-        </li>
-        <li>
-            <a href="#" class="image"><img src="css/images/pq21538799.jpg" alt="Game" /></a>
-        	<h2>Prototype</h2>
-            <p>R$ 135,00</p>
-        </li>
-        <li>
-            <a href="#" class="image"><img src="css/images/pq21435686.jpg" alt="Game" /></a>
-        	<h2>Grand Theft Auto</h2>
-            <p>R$ 120,00</p>
-        </li>
-        <li>
-            <a href="#" class="image"><img src="css/images/pq21465871.jpg" alt="Game" /></a>
-                 
-        	<h2>Devil May Cry</h2>
-            <p>R$ 90,00</p>
-        </li>
-          <li>
-              <a href="#" class="image"><img src="css/images/pq21440232.jpg" alt="Game" /></a>
-        	<h2>Need for Speed</h2>
-            <p>R$ 95,00</p>
-        </li><!-- -->
+        
       
     </ul>
         
